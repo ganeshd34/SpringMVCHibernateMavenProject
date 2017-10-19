8342e79e5ee775e97212958f51fc00a64810e7b3
Ganesh D <ganeshdevadiga34@gmail.com>
2017-10-19 23:49:23 +0530

    Initial commit

---
+++ SpringMVCHibernateMavenProject/src/main/java/com/ganesh/SpringMVCHibernateMavenProject/service/PersonService.java
@@ -0,0 +1,13 @@
+package com.ganesh.SpringMVCHibernateMavenProject.service;
+
+import java.util.List;
+
+import com.ganesh.SpringMVCHibernateMavenProject.model.Person;
+
+public interface PersonService {
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+}
